a182dcf2bb6cfab33e96f0ab49d31fbc65d33dca
james nyakundi <omarejames082@gmail.com>
2024-03-24 16:35:57 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }function writeCards(names, eventName) {
+    let thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+
+  
+  countDown(10);
+  
